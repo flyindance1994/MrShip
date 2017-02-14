7383dd4e54b7e9f29b7163d9d9763128cb57f4a3
flyindance1994 <flyindancedz@gmail.com>
2017-02-14 16:55:46 +0800

    test controller place sinovoice

---
+++ application/index/controller/Blog.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dengzhou
+ * Date: 2017/2/14
+ * Time: 16:52
+ */
+namespace app\index\controller;
+
+use think\Controller;
+
+class Blog extends Controller
+{
+    public function index()
+    {
+        echo "Hello";
+    }
+}
