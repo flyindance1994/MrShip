516be1c5d7415c6b524d79b1cff39619ccbb3bb7
flyindance1994 <flyindancedz@gmail.com>
2017-02-17 17:20:53 +0800

    test code.html place sinovoice

---
+++ application/index/controller/Code.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dengzhou
+ * Date: 2017/2/17
+ * Time: 15:32
+ */
+namespace app\index\controller;
+
+use think\Controller;
+
+class Code extends Controller{
+    public function index(){
+        return view();
+    }
+}
