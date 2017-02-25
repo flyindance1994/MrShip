14ef51e89be323b90db78ddc91ab831f4d9707bf
flyindance1994 <flyindancedz@gmail.com>
2017-02-25 23:59:52 +0800

    edit image type place home

---
+++ application/index/controller/Photo.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flyin
+ * Date: 2017/2/25
+ * Time: 23:28
+ */
+namespace app\index\controller;
+
+use think\Controller;
+use think\View;
+
+class Photo extends Controller
+{
+    public function index()
+    {
+        return view();
+    }
+}
